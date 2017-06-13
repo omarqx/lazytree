02a7230786095b5219b82e57687906633429f63e
omarqa <omar.qaddoumi@am.sony.com>
2017-06-12 18:40:31 -0700

    feat(tree): generate missing ids
    added the feature were if there are no id present for each item, it'll autogenerate a uuid that will
    be bounded to element id

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+}
+
+
 function createTextElement(type, text, id) {
   const element = document.createElement(type);
   element.appendChild(document.createTextNode(text));
-  element.setAttribute('id', id);
+  element.setAttribute('id', (id === undefined) ? guid() : id);
   return element;
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,16 @@
 const treeview = require('../src/index');
 
 describe('basic list', () => {
-  it('should render basic two elements correctly', () => {
+  it('should render single element correctly', () => {
+    const tree = treeview([
+      {
+        value: 'first',
+        id: 1
+      }
+    ]);
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render two elements correctly', () => {
     const tree = treeview([
       {
         value: 'first',
@@ -14,13 +23,16 @@ describe('basic list', () => {
     ]);
     expect(tree).toMatchSnapshot();
   });
-  it('should render basic single element correctly', () => {
-    const tree = treeview([
-      {
-        value: 'first',
-        id: 1
-      }
-    ]);
+  it('should render empty list correctly', () => {
+    const tree = treeview([]);
     expect(tree).toMatchSnapshot();
+  });
+  it('should render list with uniqe id', () => {
+    const tree = treeview([{
+      value:'no id generate guid',
+    }]);
+    const id = tree.children[0].getAttribute('id');
+    expect(id).not.toBe(null);
+    expect(id).toMatch(/^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/);
   });
 });
